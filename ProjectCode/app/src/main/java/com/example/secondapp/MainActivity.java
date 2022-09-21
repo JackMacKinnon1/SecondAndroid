c3277d59832ddfc783fcbab48278565615756ddb
w0449149 <w0449149@campus.nscc.ca>
2022-09-21 13:25:51 -0300

    our activity

---
+++ ProjectCode/app/src/main/java/com/example/secondapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.secondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    SeekBar billEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        billEditText.findViewById(R.id.billEditText);
+    }
+}
